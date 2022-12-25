625f39ba7a2bc8d772852edc4fca01ee250aa522
shivaAg04 <81668752+shivaAg04@users.noreply.github.com>
2022-12-25 16:59:15 +0530

    login done

---
+++ lib/screen/games/mcqGame.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:speech_app/screen/games/m2.dart';
+import 'package:video_player/video_player.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+class McqGame extends StatefulWidget {
+  const McqGame({super.key});
+
+  @override
+  State<McqGame> createState() => _McqGameState();
+}
+
+class _McqGameState extends State<McqGame> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsFlutterBinding.ensureInitialized();
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.bottom]);
+    _controller = VideoPlayerController.asset('assets/videos/animal.mp4')
+      ..initialize().then((_) {
+        _controller.play();
+        _controller.setLooping(true);
+        // Ensure the first frame is shown after the video is initialized
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        splashColor: Colors.red,
+        child: ClipRRect(child: Image.asset("assets/next.png")),
+        onPressed: () {
+          // SystemSound.play(SystemSoundType.click);
+          AudioPlayer().play(AssetSource("audio/tap.mp3"));
+          Navigator.of(context)
+              .pushReplacement(MaterialPageRoute(builder: (context) {
+            return m2();
+          }));
+        },
+      ),
+      body: Stack(
+        children: [
+          SizedBox.expand(
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: SizedBox(
+                width: _controller.value.size.width,
+                height: _controller.value.size.height,
+                child: VideoPlayer(_controller),
+              ),
+            ),
+          ),
+          // bg(context),
+          Image.asset(
+            alignment: Alignment.topCenter,
+            "assets/A.gif",
+            height: 125.0,
+            width: 125.0,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Image.asset("assets/images/point.png"),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      AudioPlayer().play(AssetSource("audio/correct.mp3"));
+                      rightDialog(context, "Apple");
+                    },
+                    child: Image.asset(
+                      "assets/images/apple.png",
+                      height: 125.0,
+                      width: 125.0,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      AudioPlayer().play(AssetSource("audio/wrong.mp3"));
+                      wrongDialog(context, "Orange");
+                    },
+                    child: Image.asset(
+                      "assets/images/orange.png",
+                      height: 125.0,
+                      width: 125.0,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      AudioPlayer().play(AssetSource("audio/wrong.mp3"));
+                      wrongDialog(context, "Guava");
+                    },
+                    child: Image.asset(
+                      "assets/images/guavava.png",
+                      height: 125.0,
+                      width: 125.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<dynamic> wrongDialog(BuildContext context, String name) {
+    return showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        backgroundColor: Color.fromARGB(255, 183, 255, 185),
+        title: const Text(
+          "Wrong Answer",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        ),
+        content: Text("No this is $name"),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+            child: Container(
+              padding: const EdgeInsets.all(14),
+              child: const Text("okay", style: TextStyle(color: Colors.red)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<dynamic> rightDialog(BuildContext context, String name) {
+    return showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (ctx) => AlertDialog(
+        backgroundColor: Color.fromARGB(255, 183, 255, 185),
+        title: const Text(
+          "Hurray Right Answer",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        ),
+        content: Text("This is $name"),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(ctx)
+                  .pushReplacement(MaterialPageRoute(builder: (context) {
+                return m2();
+              }));
+            },
+            child: Container(
+              padding: const EdgeInsets.all(14),
+              child: const Text("next", style: TextStyle(color: Colors.red)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container bg(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      // Below is the code for Linear Gradient.
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.green, Color.fromARGB(255, 0, 255, 247)],
+          begin: Alignment.center,
+          end: Alignment.topRight,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen/login/login.dart
@@ -0,0 +1,112 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+// import 'package:kiet_olx/screens/User/sign_in.dart';
+// import 'package:kiet_olx/screens/User/sign_up.dart';
+import 'package:lottie/lottie.dart';
+import 'package:speech_app/screen/login/main_user_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  bool isloggedin = false;
+  checkAuthentification() async {
+    _auth.authStateChanges().listen((user) {
+      if (user != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return const MainUserScreen();
+          }),
+        );
+      }
+    });
+  }
+
+  googleLogin() async {
+    print("googleLogin method Called");
+
+    try {
+      var googleAc = await _googleSignIn.signIn();
+      if (googleAc == null) {
+        return;
+      }
+
+      final userData = await googleAc.authentication;
+      final credential = GoogleAuthProvider.credential(
+          accessToken: userData.accessToken, idToken: userData.idToken);
+
+      await FirebaseAuth.instance.signInWithCredential(credential);
+
+      checkAuthentification();
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Image.asset(
+          "assets/images/bgImage.png",
+          height: (MediaQuery.of(context).size.height),
+          width: (MediaQuery.of(context).size.width),
+          fit: BoxFit.cover,
+          filterQuality: FilterQuality.high,
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 190, 20, 30),
+          child: Lottie.asset('assets/lottie/panda.json'),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(70, 190, 40, 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                    text: 'Welcome to ',
+                    style: TextStyle(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'OLX kiet',
+                          style: TextStyle(
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.green))
+                    ]),
+              ),
+              const SizedBox(height: 10.0),
+              const Text(
+                textAlign: TextAlign.center,
+                'Sell your unused Product here',
+                style: TextStyle(color: Colors.black),
+              ),
+              const SizedBox(height: 30.0),
+              SignInButton(
+                  elevation: 10,
+                  Buttons.Google,
+                  text: "Sign up with E-MAIL ID",
+                  onPressed: googleLogin)
+            ],
+          ),
+        ),
+      ],
+    ));
+  }
+}
